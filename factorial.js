4b3b0d95ba027a0b3446319eb01bac7432da71c2
Olivia <olivia.akhmaeva@gmail.com>
2020-09-27 21:29:11 +0300

    fact and farm

---
+++ factorial.js
@@ -0,0 +1,12 @@
+
+console.log('Count the factorial of 10: ');
+function factorial(n)
+{
+  {
+    if (n==1) return 1;
+    else return n*factorial(n-1);		
+    } 
+}
+console.log(factorial(10));
+
+
